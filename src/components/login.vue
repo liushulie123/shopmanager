f265c554f1729bf3a4e8570977d3c995e2a51704
liushulie123 <2726277856@qq.com>
2019-02-13 22:54:14 +0800

    登陆-引入表单组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,29 @@
 <template>
-    <div>
-        login
-        -----
+    <div class="login-wrap">
+        <el-form :label-position="top" label-width="80px" :model="formdata">
+            <el-form-item label="用户名">
+                <el-input v-model="formdata.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="formdata.password"></el-input>
+            </el-form-item>
+              <el-button class="login-btn" type="primary">登陆</el-button>
+        </el-form>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            formdata:{
+                username:'',
+                password:''
+            }
+        }
+    },
 }
 </script>
 
 <style>
-
 </style>

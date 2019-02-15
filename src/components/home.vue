f38a1a37b8915f06e795331ec31658a549ed8251
liushulie123 <2726277856@qq.com>
2019-02-15 16:58:54 +0800

    home 完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,8 +1,89 @@
 <template>
   <el-container class="container">
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="@/assets/logo.png" alt="图片加载失败">
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">
+            <a href="#" @click.prevent="handleLoginout()" class="loginout">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-aside class="aside" width="200px">
+        <!-- 导航 -->
+        <!-- router :开启路由模式,点击对应的导航,当前的path就是激活导航的index值 -->
+        <el-menu default-active="1" :router="true" :unique-opened="true">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-service"></i>用户列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-service"></i>角色列表</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-service"></i>权限列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-service"></i>商品列表</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-service"></i>分类参数</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-service"></i>商品分类</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-service"></i>订单列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-service"></i>数据报表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
@@ -10,19 +91,51 @@
 
 <script>
 export default {
+  // 如果用户没有登陆,/标识匹配到时,不能进入home.vue->显示login.vue
+  // if(有没有正确的token){渲染home.vue}
+  // if(没有token){改login,显示login.vue}
+  // 代码位置,在渲染home.vue之前  当home.vue组件渲染完毕时->mounted()
+  // beforeMount(){}
+  beforeMount() {
+    if (!localStorage.getItem('token')) {
+      this.$router.push({
+        name: 'login'
+      })
+    }
+  },
+  methods: {
+    handleLoginout() {
+      // 1.清除token
+      localStorage.clear()
+      // 2.跳转到Login
+      this.$router.push({
+        name: "login"
 
+      })
+      //提示
+      this.$message.warning('退出成功')
+    }
+  },
 }
 </script>
 
 <style>
-  .container{
-      height: 100%;
-      background: yellow;
-  }
-  .aside{
-      background: red;
-  }
-  .main{
-      background: gray;
-  }
+.container {
+  height: 100%;
+  background: #b3c0d1;
+}
+.aside {
+  background: #fff;
+}
+.main {
+  background: gray;
+}
+.middle {
+  text-align: center;
+  line-height: 60px;
+}
+.loginout {
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,23 +1,21 @@
 <template>
-    <div class="login-wrap">
-        <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
-            <h2>用户登录</h2>
-            <el-form-item label="用户名">
-                <el-input v-model="formdata.username"></el-input>
-            </el-form-item>
-            <el-form-item label="密码">
-                <el-input v-model="formdata.password"></el-input>
-            </el-form-item>
-              <el-button
-              @click.prevent = "handlelogin()"
-              class="login-btn" type="primary">登陆</el-button>
-        </el-form>
-    </div>
+  <div class="login-wrap">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button @click.prevent="handlelogin()" class="login-btn" type="primary">登陆</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       formdata: {
         username: "",
@@ -26,24 +24,19 @@ export default {
     }
   },
   methods: {
-    async handlelogin () {
-        // 目前代码：异步的结果res 在一个函数里面获取的
-        // 目的：res的获取是同步
+    async handlelogin() {
+      // 目前代码：异步的结果res 在一个函数里面获取的
+      // 目的：res的获取是同步
       const res = await this.$http.post(`login`, this.formdata)
-      const {
-        data: {
-          data:{token},
-          meta: {msg, status}
-        }
-      } = res
-      if(status === 200) {
+      const { data: { data, meta: { msg, status } } } = res
+      if (status === 200) {
         // 提示：token这个值目前不需要关心。 将来要用。把token永久存储
         // session/cookie/localStorage(Html5新特性)
         // key名:要存储的数据
-        localStorage.setItem('token',token)
+        localStorage.setItem("token", data.token)
         this.$router.push({
-          name:"home"
-        })
+          name: "home"
+        });
       } else {
         this.$message.error(msg)
       }
@@ -67,7 +60,7 @@ this.$http
             this.$message.error(msg)
           }
         })
-*/ 
+*/
 </script>
 <style>
 .login-wrap {
